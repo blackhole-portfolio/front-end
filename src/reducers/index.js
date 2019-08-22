c8ce7c9a20adec6214a549a649062cf34ea618ca
Christian-Ford <fjord.christian@gmail.com>
2019-08-21 22:39:59 -0500

    Imported all actions into reducers index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,21 @@
 import {
-
+    USER_UNAUTHORIZED ,
+    LOGIN_START       ,
+    LOGIN_SUCCESS     ,
+    LOGOUT            ,
+    REG_START         ,
+    REG_SUCCESS       ,
+    FETCH_DATA_START  ,
+    FETCH_DATA_SUCCESS,
+    FETCH_DATA_FAILURE,
+    GET_USERS_START   ,
+    GET_USERS_SUCCESS ,
+    GET_USERS_FAILURE ,
+    ADD_START         ,
+    ADD_SUCCESS       ,
+    DELETE_START      ,
+    DELETE_SUCCESS    ,
+    DELETE_FAILURE    ,
+    EDIT_START        ,
+    EDIT_SUCCESS
 } from '../actions/index';
