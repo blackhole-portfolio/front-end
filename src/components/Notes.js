0d89b1866ae1843a56b0824fe0c778c9280d9799
Christian-Ford <fjord.christian@gmail.com>
2019-08-22 18:52:50 -0500

    Functionality for state | back button | update button

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -7,3 +7,36 @@ import { connect } from 'react-redux';
 import { deleteNotes, updateNotes } from '../actions/index';
 
 import EditForm from './EditForm';
+
+// Notes class
+
+class Notes extends React.Component {
+    // state
+    state = {
+        animate: false ,
+        updatingNoteId: null
+    }
+
+    // Back button functionallity
+    back = e => {
+        e.preventDefault();
+
+        this.props.history.push('/main-page');
+    };
+
+    // Update Note functionallity
+    updateNote = (e, note) => {
+        e.preventDefault();
+
+        this.props.updateNote(note);
+
+        setTimeout( () => 
+        this.setState({
+            updatingNOteID: null
+        }), 300)
+
+        setTimeout( () => {
+            this.props.history.push('/main-page')
+        }, 400)
+    }
+};
