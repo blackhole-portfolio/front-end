cbf513996ed093a9e1082d784bff6efd537bf6d7
Christian-Ford <fjord.christian@gmail.com>
2019-08-22 18:47:34 -0500

    Dependencies imported to Notes.js

---
+++ src/components/Notes.js
@@ -0,0 +1,9 @@
+// Import all dependencies
+
+import React from 'react';
+import moment from 'moment';
+
+import { connect } from 'react-redux';
+import { deleteNotes, updateNotes } from '../actions/index';
+
+import EditForm from './EditForm';
