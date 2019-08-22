9658f0601fbbd771b68c84a6ac8e19a2fe6051f9
Christian-Ford <fjord.christian@gmail.com>
2019-08-21 21:01:54 -0500

    Login endpoint axios call complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,21 @@
 import axios from 'axios';
+
+// Login endpoint connection
+
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+
+export const login = creds => dispatch => {
+    dispatch({ type: LOGIN_START });
+
+    axios.post("https://blackhole-backend.herokuapp.com/api/auth/login", creds)
+        .then( res => {
+            console.log(res.data)
+
+            localStorage.setItem('token', res.data.token)
+            localStorage.setItem('user_id', res.data.user.id)
+
+            dispatch({ type: LOGIN_SUCCESS , payload: res.data.token })
+
+        });
+};
