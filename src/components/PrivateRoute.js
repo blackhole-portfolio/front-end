d5d643632b4718c32fa26368bb5ed89741368907
Christian-Ford <fjord.christian@gmail.com>
2019-08-23 12:24:47 -0500

    Private Route Redirect finished

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,41 @@
+// Imports
+
+import React from "react";
+import { Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+
+const PrivateRoute = ({
+  component: Component,
+  token,
+  errorStatusCode,
+  ...rest
+}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        token && errorStatusCode !== 403 ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+};
+
+// State Mapping
+
+const mapStateToProps = ({ token, errorStatusCode }) => ({
+  token,
+  errorStatusCode
+});
+
+// Export
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    {}
+  )(PrivateRoute)
+);
