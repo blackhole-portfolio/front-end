8d6d77b1aea0ceb344b51b490b0d017d2c53c4e0
Christian-Ford <fjord.christian@gmail.com>
2019-08-23 12:21:11 -0500

    Finished Notes.js

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,53 +1,97 @@
-// Import all dependencies
+// Dependencies
 
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { deleteNotes, updateNotes } from '../actions';
 import moment from 'moment';
-
-import { connect } from 'react-redux';
-import { deleteNotes, updateNotes } from '../actions/index';
-
 import EditForm from './EditForm';
 
-// Notes class
+// Class creation for Notes
 
-class Notes extends React.Component {
-    // state
-    state = {
-        animate: false ,
-        updatingNoteId: null
-    }
+class Notes extends React.Component{
 
-    // Back button functionallity
-    back = e => {
-        e.preventDefault();
+  // Set State
 
-        this.props.history.push('/main-page');
-    };
+  state = {
+    animate: false,
+    updatingNoteId: null
+  }
+  
+   // Back Button
 
-    // Update Note functionallity
-    updateNote = (e, note) => {
-        e.preventDefault();
+   back = e => {
+    e.preventDefault();
+    this.props.history.push("/main-page");
+  };
 
-        this.props.updateNote(note);
+  // Update Notes
 
-        setTimeout( () => 
-        this.setState({
-            updatingNOteID: null
-        }), 300)
+  updateNote = (e, note) => {
+    e.preventDefault();
+    this.props.updateNotes(note);
+    
+    setTimeout(() =>   this.setState({
+      updatingNoteId: null
+    }), 300)
+    setTimeout(() => {this.props.history.push('/main-page')}, 400)
+  };
+  
+  // Handle Animations
 
-        setTimeout( () => {
-            this.props.history.push('/main-page')
-        }, 400)
-    }
+  handleAnimate = (id) => {
+    this.setState(prevState => ({
+      animate: !prevState.animate
+    }));
+   setTimeout(() => {this.props.deleteNotes(id);}, 3000)
+   setTimeout(() => {this.props.history.push('/main-page')}, 3200)
+  };
+  
 
-    // Animation properties
-    handleAnimate = (id) => {
-        this.setState(prevState => ({
-            animate: !prevState.animate
-        }));
 
-    setTimeout( () => { this.props.deleteNotes(id); }, 3000)
 
-    setTimeout( () => { this.props.history.push('/main-page') }, 3200)
-    };
-};
+ // Render
+ render() {
+     const note = this.props.notes.find(
+    note => `${note.id}` === this.props.match.params.id);
+    if(this.state.updatingNoteId === note.id) {
+      return <EditForm note={note} updateNote={this.updateNote} updatingNotes={this.props.updatingNotes} handleAnimate={this.handleAnimate} />
+    } 
+
+   // REturn
+
+   return (
+     <div className='note-container'>
+    <div className={this.state.animate ? 'gone' : 'single-note'}>
+    <div className="single-text">
+      <h3> {note.message}</h3>
+      <h5> Time Until Destruction: {moment().add(note.delete_at, 'day').fromNow(true)} </h5>
+    </div>
+    <div className="single-btn">
+     <button onClick={() => this.setState({ updatingNoteId: note.id })}>Edit</button>
+     <button onClick={() => this.handleAnimate(note.id)}>Blackhole</button>
+     <button onClick={this.back}>Back</button>
+   </div>
+  </div>
+  </div>
+  
+   
+   )
+ }
+
+}
+
+  
+// Map State
+
+const mapStateToProps = state => ({
+  notes: state.notes,
+  deletingNotes: state.deletingNotes,
+  updatingNotes: state.updatingNotes
+});
+
+// Export 
+
+export default connect(
+  mapStateToProps,
+  { deleteNotes, updateNotes }
+)(Notes);
